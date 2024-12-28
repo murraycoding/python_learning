732f4c8f8b0b2333e57fd58ce26b06e8a7631e95
Brian Murray <murraycoding@gmail.com>
2024-12-28 07:38:55 -0500

    added assignment 1 to the branch

---
+++ assignments/assignment_1.py
@@ -0,0 +1,18 @@
+"""
+Assignment 1: Python Basics
+Goal: Understand how to use variables and basic data types in Python
+
+1. Define two variables called num1 and num2 and assign them the values 10 and 20, respectively.
+2. Print the sum of num1 and num2.
+3. Print the difference when subtracting num1 from num2.
+4. Print the product of num1 and num2.
+5. Print the result of num1 divided by num2.
+
+6. Update the values of num1 and num2 to 15 and 25, respectively but make them strings.
+7. Add a type conversion to the code to make sure the variables are integers before performing the arithmetic operations.
+
+8. (BONUS) Use the input() function to allow the user to enter the values of num1 and num2.
+
+"""
+
+# Code here
